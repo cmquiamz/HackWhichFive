64c6bc6c8c5bb977b61e1a76ef44535e73bd483a
CM Student <cmstudent@UHWO-ACM-20.local>
2018-03-19 11:20:55 -1000

    Completed Hackwich #7

--- HackWhichFive/ViewController.swift
+++ HackWhichFive/ViewController.swift
@@ -14,30 +14,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet var tableView: UITableView!
     var greetingArray = ["Hello", "Aloha", "Good Morning", "Howzit", "Sup"]
+    
+    var kapoleiRestaurantArray = ["Kapolei Kalapawai"]
+    
+    var restaurantImageData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     //Set VC as delegate
-        //
         
         self.tableView.dataSource = self
+        
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        restaurantImageData = dict!.object(forKey: "restaurantImages") as! [String]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return greetingArray.count
+        return kapoleiRestaurantArray.count
 }
+    
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     //set up cell to display items in greetingArray
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        let text = greetingArray[indexPath.row]
-        cell.textLabel?.text = text
-        return cell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+    
+    let text = kapoleiRestaurantArray[indexPath.row]
+    
+    cell.textLabel?.text = text
+    
+    return cell
 }
+    
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "mySegue"
+    {
+        let s1 = segue.destination as! detailViewController
+        let imageIndex = tableView.indexPathForSelectedRow?.row
+        s1.imagePass = restaurantImageData[imageIndex!]
+        }
+    
+    }
+    
     
 }
 
